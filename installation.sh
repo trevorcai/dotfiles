3abf37feaca745f005f692b31a13caf03d9cc2a6
Trevor Cai <trevorcai@users.noreply.github.com>
2019-10-23 01:52:24 +0100

    add tldr, tmux to setup script

--- installation.sh
+++ installation.sh
@@ -6,9 +6,14 @@ echo 'Installation script for macOS.'
 echo 'Press enter to continue after each prompt.'
 read -p 'Install Homebrew.' unused
 
-echo 'Installing RipGrep...'
-brew install ripgrep
-
 echo 'Installing FZF...'
 brew install fzf
 $(brew --prefix)/opt/fzf/install
+
+echo 'Installing RipGrep...'
+brew install ripgrep
+
+echo 'Installing tldr...'
+brew install tldr
+
+read -p 'Run: which tmux || brew install tmux.' unused
