d7d91a62049e311a762a5596cd9870f446d21d15
Trevor Cai <trevorcai@users.noreply.github.com>
2019-11-04 02:22:41 +0000

    Migrate to GNU stow for management

--- installation.sh
+++ installation.sh
@@ -6,14 +6,10 @@ echo 'Installation script for macOS.'
 echo 'Press enter to continue after each prompt.'
 read -p 'Install Homebrew.' unused
 
-echo 'Installing FZF...'
-brew install fzf
+echo 'Installing fzf, ripgrep, stow, tldr'
+brew install fzf ripgrep stow tldr
+
+echo 'Setting up fzf...'
 $(brew --prefix)/opt/fzf/install
-
-echo 'Installing RipGrep...'
-brew install ripgrep
-
-echo 'Installing tldr...'
-brew install tldr
 
 read -p 'Run: which tmux || brew install tmux.' unused
