17b20095659793f848af505fd552e840b5c7e57c
Trevor Cai <trevorycai@gmail.com>
2025-06-24 23:10:50 -0700

    Update autoinstall script

--- installation.sh
+++ installation.sh
@@ -4,10 +4,12 @@ set -e
 
 echo 'Installation script for macOS.'
 echo 'Press enter to continue after each prompt.'
-read -p 'Install Homebrew.' unused
+
+echo 'Installing homebrew.'
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'Installing useful utilities.'
-brew install bat eza fd fzf htop ripgrep stow tldr tmux tree
+brew install bat eza fd fzf htop ripgrep stow tlrc tmux tree
 
 echo 'Setting up fzf...'
 $(brew --prefix)/opt/fzf/install
@@ -15,11 +17,14 @@ $(brew --prefix)/opt/fzf/install
 echo 'Installing some symlinks using stow...'
 stow --target=$HOME git tmux vim zsh
 
+echo 'Installing cursor and zed.'
+brew install --cask cursor zed
+
+echo ' Installing zed config.'
 ZED_DIR="$HOME/.config/zed"
 mkdir -p "$ZED_DIR"
 stow --target="$ZED_DIR" zed
 
-read -p 'Install Cursor.' unused
-echo 'Linking Cursor settings and keybindings in.'
+echo 'Linking Cursor settings.'
 CURSOR_DIR="$HOME/Library/Application Support/Cursor/User"
 rm -f "$CURSOR_DIR/settings.json" && stow --target="$CURSOR_DIR" cursor
